59b608e589ffb9cc7086797fd43c4f02b20142ce
SBALDOCK <stephenbaldock78@gmail.com>
2020-09-15 16:57:33 -0700

    Updates

---
+++ precache-manifest.e7820157e5d4c5dbdff04116028ebe47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72c6c5b2cb9e856652a06c5eb18d5cf",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "d4bb2f6c47008580d9d7",
+    "url": "/resty/static/css/main.7b20ec8f.chunk.css"
+  },
+  {
+    "revision": "d073437c50cd268abfbf",
+    "url": "/resty/static/js/2.5f83abb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.5f83abb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bb2f6c47008580d9d7",
+    "url": "/resty/static/js/main.476f20fa.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
