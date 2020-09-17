786d259f9d61765609e26a431f176869ff430b4d
SBALDOCK <stephenbaldock78@gmail.com>
2020-09-17 08:25:34 -0700

    broken but submitting updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import md5 from 'md5';
 import './App.css';
 import './app.scss';
 import Header from './components/Header/header.js';
@@ -7,27 +9,71 @@ import Form from './components/Form/form.js';
 import Results from './components/results/results.js';
 import History from './components/history/history.js';
 
+function App(props) {
+  const [loading, toggleLoading] = useState(false);
+  const [results, fetchResults] = useState(null);
+  const [history, updateHistory] = useState(null);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: null,
-      headers: null,
-    };
+
+  // This is saying, if this function is called (in fetch) and there is no loading state, then run the function
+  toggleLoading = () => {
+    this.setState({ loading: !this.state.loading });
   }
 
-  handleForm = (results, headers) => {
-    this.setState({ results, headers });
+  // This is saying, take search results, convert (stringify) and place in local storage
+  // MD5 is a hashing algorithm that is no longer widely used - too many exposed hash tables
+  updateHistory(request) {
+    let hash = md5(JSON.stringify(request));
+
+    const history = { ...this.state.history, [hash]: request }
+
+    this.setState({ history }, () => {
+      localStorage.setItem('history', JSON.stringify(this.state.history));
+    });
+  }
+
+  // This is setting state for result to be returned from API call
+  updateResults = (headers, results) => {
+    this.setState({ headers, results });
   };
 
+  // This is a function that initiates changing the form to reflect what method requested
+  updatedRequest = (request) => {
+    this.setState({ request });
+  }
+
+  //this handles the submit, triggering the appropriate functions listed below asynchronously or sends back an error
+  fetchResults = async (request) => {
+    try {
+      this.toggleLoading();
+      this.updateRequest(request);
+      let response = await axios(request);
+      this.toggleLoading();
+      this.updateHistory(request);
+      this.updateResults(response.headers, response.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  // Waits for component to mount to DOM or render
+
+  useEffect(() => {
+    let history = JSON.parse(localStorage.getItem('history'));
+      this.setState({ history });
+  },
+
+  // Run fetchResults handler to trigger all functions above
   render() {
     return (
       <>
         <Header />
-        <Form handler={this.handleForm} />
-        <History />
-        <Results results={this.state.results} headers={this.state.headers} />
+        <Form request={this.state.request} handler={this.fetchResults} />
+        <main>
+          <History handler={this.updateRequest} calls={this.state.history} />
+          <Results loading={this.state.loading} results={this.state.results} headers={this.state.headers} />
+        </main>
         <Footer />
       </>
     );
--- src/components/history/history.js
+++ src/components/history/history.js
@@ -5,25 +5,24 @@ import './history.scss';
 function  History (props){
   const calls = props.calls || {};
   function loadRequest(apiCall){
-    console.log(apiCall);
     props.handler(apiCall);
   }
 
   return (
-  <aside className="history">
-    <ul>
-      {
-        Object.keys(calls).map(key =>
-          <li key={key}>
-            <span className={`method ${props.calls[key].method}`}>{props.calls[key].method}</span>
-            <button className="url" onClick={() => loadRequest(props.call[key])}>{props.calls[key].url}</button>
-          </li>,
-        )
-      }
-    </ul>
-  </aside>
+    <aside className="history">
+      <h4>History</h4>
+      <ul>
+        {
+          Object.keys(calls).map(key =>
+            <li key={key}>
+              <span className={`method ${props.calls[key].method}`}>{props.calls[key].method}</span>
+              <button className="url" onClick={() => loadRequest(props.call[key])}>{props.calls[key].url}</button>
+            </li>,
+          )
+        }
+      </ul>
+    </aside>
   );
-
 };
 
 export default History;
